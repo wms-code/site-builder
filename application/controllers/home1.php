fdd978d6d357a0ffd35edd8c5e899eaaa0a7e696
dreamstudio-satheesh <satheeshpdh@gmali.com>
2013-07-22 18:40:55 +0530

    add temp home

--- application/controllers/home1.php
+++ application/controllers/home1.php
@@ -10,44 +10,73 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Home extends MY_Controller {
+	
+	  public function __construct()
+       {
+            parent::__construct();
+            // Your own constructor code
+			 
+       }
 
     public function index() {
         $this->template->set('title', 'My website');
         $this->template->load('layouts/main', 'home');
     }
-
+	
 	public function build()	{
+		//assign variables
+		$data='';		
+		$data['filecontents'] ='';
+		$data['mytitle'] ='';
+		$doc = new DOMDocument();		
 		$arg = $this->uri->segment(2);
-		$data['filecontents'] = "";
-		$doc = new DOMDocument();
 		$pagetitle = $this->input->post('mytitle');
-
-		if($arg!="")
-		{
+		
+		if($arg!=" ")		
+		{		
 			$page = base_url().'themes/infocus/'.$this->uri->segment(2).'.html';
-			$data['filecontents'] = file_get_contents($page,FILE_USE_INCLUDE_PATH);
-			$doc->loadHTML($data['filecontents']);
-			$data['mytitle'] = $doc->getElementsByTagName("title")->item(0)->textContent;
 
-			if($pagetitle)
-			{
+			$data['filecontents'] = file_get_contents($page);
+			$doc->loadHTML($data['filecontents']);
+			//check  head tag avail...
+			if($doc->getElementsByTagName("head")->item(0))
+				{
+				//check if title tag available
+				if($doc->getElementsByTagName("title")->item(0) && $pagetitle)
+				{
+				$data['mytitle'] = $doc->getElementsByTagName("title")->item(0)->textContent;
 				$writepage = 'themes/infocus/'.$this->uri->segment(2).'.html';
 				$parent = $doc->getElementsByTagName("title")->item(0);
 				$newchild = $doc->createTextNode($pagetitle);
 				$parent->replaceChild($newchild, $parent->firstChild);
 				$doc->saveHTMLFile($writepage);
 				$data['mytitle'] = $pagetitle;
-			}		
-
+				}
+				else if($pagetitle)
+				{
+					
+					echo "('This html document does not have header', 'page:home.php function:build.')";
+					exit();
+				}
+			
+			}
+			elseif($doc->hasChildNodes())
+			{
+			
+			
+			}
+		
+					
+		
 		}
-
+		
 		$this->template->set('title','Website builder');
 		$this->template->load('layouts/main','build',$data);
 	}
-
+	
 	public function titlechange()
 	{
-
+	
 	 $this->template->set('title','Change Title');
 	 $this->template->load('layouts/main','titlechange');
 	}
--- application/views/build.php
+++ application/views/build.php
@@ -1,10 +1,13 @@
-<h1>Welcome</h1>
-<form method='post'>
-Page Title: <input type='text' name='mytitle' value="<?php echo $mytitle ?>" /> 
-<br/><input type="submit" class="btn btn-info" />
-</form>
+
 <div class="well">
-	<p>Add your awesome content here!</p>
+	<form method='post'>
+	 <fieldset>
+	 <legend>Legend</legend>
+    <label>Page Title: </label>
+<input type='text' name='mytitle' value="<?php echo $mytitle ?>" /> 
+<br/><input type="submit" class="btn btn-info" />
+</fieldset>
+</form>
 </div>
 <textarea id="myedit">
 	<?php echo $filecontents ?>
