076b62d588a38de9fd64b7e87d074ff833392b8d
dreamstudio-satheesh <satheeshpdh@gmali.com>
2013-07-23 20:36:30 +0530

    change home page
    change home page and ckedtior ctrl+s

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,39 +10,117 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Home extends MY_Controller {
+	
+	  public function __construct()
+       {
+            parent::__construct();
+            // Your own constructor code
+			 
+       }
 
     public function index() {
         $this->template->set('title', 'My website');
         $this->template->load('layouts/main', 'home');
     }
+	
+	function fetchUrl($uri) {
+    $handle = curl_init();
 
+    curl_setopt($handle, CURLOPT_URL, $uri);
+    curl_setopt($handle, CURLOPT_POST, false);
+    curl_setopt($handle, CURLOPT_BINARYTRANSFER, false);
+    curl_setopt($handle, CURLOPT_HEADER, true);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 10);
+
+    $response = curl_exec($handle);
+    $hlength  = curl_getinfo($handle, CURLINFO_HEADER_SIZE);
+    $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    $body     = substr($response, $hlength);
+
+    // If HTTP response is not 200, throw exception
+    if ($httpCode != 200) {
+        throw new Exception($httpCode);
+    }
+
+    return $body;
+	}
 	public function build()	{
-		$arg = $this->uri->segment(2);
-		$data['filecontents'] = "";
-		$doc = new DOMDocument();
-		$pagetitle = $this->input->post('mytitle');
-
-		if($arg!="")
+		if($this->input->post('myedit'))
 		{
-			$page = base_url().'themes/infocus/'.$this->uri->segment(2).'.html';
-			$data['filecontents'] = file_get_contents($page,FILE_USE_INCLUDE_PATH);
-			$doc->loadHTML($data['filecontents']);
-			$data['mytitle'] = $doc->getElementsByTagName("title")->item(0)->textContent;
-
-			if($pagetitle)
-			{
-				$writepage = 'themes/infocus/'.$this->uri->segment(2).'.html';
-				$parent = $doc->getElementsByTagName("title")->item(0);
-				$newchild = $doc->createTextNode($pagetitle);
-				$parent->replaceChild($newchild, $parent->firstChild);
-				$doc->saveHTMLFile($writepage);
-				$data['mytitle'] = $pagetitle;
-			}		
-
+		echo "done";
 		}
+		else
+		{
+			//assign variables
+			$data='';		
+			$data['filecontents'] ='';
+			$data['mytitle'] ='';
+			$doc = new DOMDocument();		
+			$arg = $this->uri->segment(2);
+			$pagetitle = $this->input->post('mytitle');		
+			try 
+			{
+			$page = base_url().'themes/infocus/'.$this->uri->segment(2).'.html';
+			$writepage = 'themes/infocus/'.$this->uri->segment(2).'.html';
+			libxml_use_internal_errors(true);
+			$pageload=$this->fetchUrl($page);
+			$doc->loadHTML($pageload);
+			$data['filecontents']=htmlspecialchars($pageload); 
+			libxml_clear_errors();
+				//check page name is Avil..
+				if($arg!="")		
+				{	
+					//check if title tag available
+					if($doc->getElementsByTagName("title")->item(0))
+					{
+					//get page title
+					$data['mytitle'] = $doc->getElementsByTagName("title")->item(0)->textContent;	
+						if($pagetitle!="")
+						{
+						$parent = $doc->getElementsByTagName("title")->item(0);
+						$newchild = $doc->createTextNode($pagetitle);
+						$parent->replaceChild($newchild, $parent->firstChild);
+						$doc->saveHTMLFile($writepage);
+						$data['mytitle']=$pagetitle;					
+						}
+					
+					}	
+					else if($doc->getElementsByTagName("head")->item(0))
+					{
+							if($pagetitle!="")
+							{
+							$head = $doc->getElementsByTagName("head")->item(0);	
+							$title = $doc->createElement('title');
+							$title = $head->appendChild($title);
+							$text = $doc->createTextNode($pagetitle);
+							$text = $title->appendChild($text);
+							$doc->saveHTMLFile($writepage);
+							$data['mytitle']=$pagetitle;				
+							}
+							
+					}		
+					else
+					{
+					//No head tag
+					$data['filecontents']="no header file";
+					}
+						
+				
+				}
+			}
+			catch (Exception $e) {
+			error_log('Fetch URL failed: ' . $e->getMessage() . ' for ' . $page);
+			}		
+			$this->template->set('title','Website builder');
+			$this->template->load('layouts/main','build',$data);
+		}
+		
+	}	
+	public function savefile()
+	{
+	echo "oooooooooooook";
 
-		$this->template->set('title','Website builder');
-		$this->template->load('layouts/main','build',$data);
 	}
 
 	public function titlechange()
@@ -51,6 +129,7 @@ class Home extends MY_Controller {
 	 $this->template->set('title','Change Title');
 	 $this->template->load('layouts/main','titlechange');
 	}
+	
 }
 
 /* End of file home.php */
--- application/views/build.php
+++ application/views/build.php
@@ -1,21 +1,38 @@
 
 <div class="well">
-	<form method='post'>
-	 <fieldset>
-	 <legend>Legend</legend>
-    <label>Page Title: </label>
-<input type='text' name='mytitle' value="<?php echo $mytitle ?>" /> 
-<br/><input type="submit" class="btn btn-info" />
-</fieldset>
-</form>
+	
 </div>
-<textarea id="myedit">
+<form method="post">
+<textarea id="myedit" name="myedit">
 	<?php echo $filecontents ?>
 </textarea>
-<script>
-CKEDITOR.config.fillEmptyBlocks = false;
+<script type="text/javascript">
+
 CKEDITOR.replace( 'myedit', {
 	fullPage: true,
 	allowedContent: true
+	
+
 });
+CKEDITOR.config.fillEmptyBlocks = false;
+CKEDITOR.on('instanceReady',
+      function( evt )
+      {
+         var editor = evt.editor;
+         editor.execCommand('maximize');
+      });
+	  
+	//CKEDITOR.config.extraPlugins = 'autosave';
+	
+CKEDITOR.config.keystrokes = [
+	 
+    [ CKEDITOR.CTRL + 83, 'save' ],    // CTRL + SHIFT + L
+    
+];	  
+	 
 </script>
+
+<p>
+			<input type="submit" class="btn btn-info"  value="Submit">
+		</p>
+	</form>
